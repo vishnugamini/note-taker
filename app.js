f7df1be40ee852ac7ece9d0fc0116b86d21e1e5a
Vishnu Gamini <90185423+vishnugamini@users.noreply.github.com>
2023-03-01 21:57:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const note = require('./notes.js')
 const chalk = require('chalk')
 const yargs = require('yargs')
 
-// customize yargs version
 yargs.version('1.1.0')
 
 
-//Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
@@ -28,7 +26,6 @@ yargs.command({
     }
 })
 
-//Create remove command
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
@@ -44,7 +41,6 @@ yargs.command({
     }
 })
 
-//Create list command
 yargs.command({
     command: "list",
     describe: "list your notes",
@@ -53,7 +49,6 @@ yargs.command({
     }
 })
 
-// Create read command
 yargs.command({
     command: "read",
     describe: "Read a note",
